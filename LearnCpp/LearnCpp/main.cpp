818b8eb04f9d5f8c29bbb1b172e871c04433a5db
NickNaso <nicoladelgobbo@gmail.com>
2017-11-27 22:54:17 +0100

    Start with Xcode

---
+++ LearnCpp/LearnCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnCpp
+//
+//  Created by Nicola Del Gobbo on 27/11/17.
+//  Copyright © 2017 Nicola Del Gobbo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
